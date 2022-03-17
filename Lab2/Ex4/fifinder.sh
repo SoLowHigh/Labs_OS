a68bc543c078711f3b8fdaf77165c3269d5529db
root <root@SLH.localdomain>
2022-03-17 23:04:09 +0600

    Small change in L2E4

--- Lab2/Ex4/fifinder.sh
+++ Lab2/Ex4/fifinder.sh
@@ -1,13 +1,24 @@
 if [ -f $1 ]
+
 	then
 		if test -r $1
+		
 			then echo Файл доступен на чтение
+			
 		fi
+		
 		if test -w $1
+		
 			then echo Файл доступен на запись
+			
 		fi
+		
 		if test -x $1
+		
 			then echo Файл доступен на запуск
+			
 		fi
+		
 	else echo Файл не найден
+	
 fi
